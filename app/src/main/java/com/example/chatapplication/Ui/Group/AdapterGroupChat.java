254747c62b1b9df0c9094fa60dd65d90c14d6941
Kadek Sumendra <sumendrakadek108@gmail.com>
2020-03-25 01:19:05 +0700

    display chat group

--- app/src/main/java/com/example/chatapplication/Ui/Group/AdapterGroupChat.java
+++ app/src/main/java/com/example/chatapplication/Ui/Group/AdapterGroupChat.java
@@ -2,6 +2,7 @@ package com.example.chatapplication.Ui.Group;
 
 import android.content.Context;
 import android.content.Intent;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +14,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.chatapplication.Data.ModelGroupChat;
 import com.example.chatapplication.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AdapterGroupChat extends RecyclerView.Adapter<AdapterGroupChat.ViewHolder> {
 
@@ -41,8 +49,13 @@ public class AdapterGroupChat extends RecyclerView.Adapter<AdapterGroupChat.View
         String groupIcon = model.getGroupIcon();
         String groupTitle = model.getGroupTitle();
 
+        holder.nameTv.setText("");
+        holder.timeTv.setText("");
+        holder.messageTv.setText("");
+
+        // tampilkan message terakhir dan waktu ketika user menutup group
+        loadLastMessage(model, holder);
         holder.groupTitleTv.setText(groupTitle);
-        holder.nameTv.setText(model.getGroupDescription());
         try{
             Picasso.get().load(groupIcon).placeholder(R.drawable.ic_default_white).into(holder.groupIconIv);
         }
@@ -58,6 +71,50 @@ public class AdapterGroupChat extends RecyclerView.Adapter<AdapterGroupChat.View
                 // send the groupId
                 intent.putExtra("groupId", groupId);
                 context.startActivity(intent);
+
+            }
+        });
+    }
+
+    private void loadLastMessage(ModelGroupChat model, ViewHolder holder) {
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Groups");
+        // limit data from last data
+        ref.child(model.getGroupId()).child("Messages").limitToLast(1).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    String message = ""+ds.child("message").getValue();
+                    String timestamp = ""+ds.child("timestamp").getValue();
+                    String sender= ""+ds.child("sender").getValue();
+
+                    Calendar cal = Calendar.getInstance(Locale.getDefault());
+                    cal.setTimeInMillis(Long.parseLong(timestamp));
+                    String dateTime = DateFormat.format("dd/MM/yyyy hh:mm aa",cal).toString();
+
+                    holder.messageTv.setText(message);
+                    holder.timeTv.setText(dateTime);
+
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+                    ref.orderByChild("uid").equalTo(sender).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                                String name = "" + ds.child("name").getValue();
+                                holder.nameTv.setText(name);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
---
+++ app/src/main/java/com/example/chatapplication/Ui/Group/GroupParticaipantActivity.java
@@ -0,0 +1,14 @@
+package com.example.chatapplication.Ui.Group;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.chatapplication.R;
+
+public class GroupParticaipantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_particaipant);
+    }
+}
